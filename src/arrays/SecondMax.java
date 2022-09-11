fbd5804346eb0a7a85601c4f948e90e11580d052
Aastha Kumari <aasthak108@gmail.com>
2022-09-11 15:55:38 +0530

    update

--- src/arrays/SecondMax.java
+++ src/arrays/SecondMax.java
@@ -4,24 +4,24 @@ public class SecondMax
 {
     public static void main(String[] args)
     {
-        int[] ipArray = {6,8,1,4,6,200,78,99,201,10,79,101,99,100};
+        int[] ipArray = {6,8,1,4,6,200,78,99,202,201,10,79,101,99,100};
         System.out.println("SecondMax: "+getSecondMax(ipArray));
     }
 
-    private static int getSecondMax(int[] ipArray)
+    private static int getSecondMax(int arr[])
     {
-        int secondMax = 0;
         int max = Integer.MIN_VALUE;
-        for(int i=0; i<ipArray.length; i++)
+        int secondMax = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++)
         {
-            if(max < ipArray[i])
+            if(max<arr[i])
             {
-                secondMax = max;
-                max = ipArray[i];
+                secondMax=max;
+                max=arr[i];
             }
-            else if(secondMax < ipArray[i] && secondMax < max)
+            else if(secondMax<arr[i] && max >arr[i])
             {
-                secondMax = ipArray[i];
+                secondMax=arr[i];
             }
         }
         return secondMax;
