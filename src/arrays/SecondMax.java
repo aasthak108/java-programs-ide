70ca5b38f897d65d44ae40bcc7e53f4fa3dd34e5
Aastha Kumari <aasthak108@gmail.com>
2022-09-11 14:40:54 +0530

    added - code for secondmax

---
+++ src/arrays/SecondMax.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class SecondMax
+{
+    public static void main(String[] args)
+    {
+        int[] ipArray = {6,8,1,4,6,200,78,99,201,10,79,101,99,100};
+        System.out.println("SecondMax: "+getSecondMax(ipArray));
+    }
+
+    private static int getSecondMax(int[] ipArray)
+    {
+        int secondMax = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<ipArray.length; i++)
+        {
+            if(max < ipArray[i])
+            {
+                secondMax = max;
+                max = ipArray[i];
+            }
+            else if(secondMax < ipArray[i] && secondMax < max)
+            {
+                secondMax = ipArray[i];
+            }
+        }
+        return secondMax;
+    }
+}
