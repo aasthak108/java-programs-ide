226c267ebc928f09e8de3a0d8ec628667cc7937f
Aastha Kumari <aasthak108@gmail.com>
2022-10-20 23:55:29 +0530

    update

--- src/searching/KthSmallestNumber.java
+++ src/searching/KthSmallestNumber.java
@@ -2,15 +2,15 @@ package searching;
 import java.util.Arrays;
 
 public class KthSmallestNumber {
-    static int kthSmallestNum(int arr[],int k)
+    static int kthSmallestNum(int array[],int k)
     {
-        Arrays.sort(arr);
-        return arr[k-1];
+        Arrays.sort(array);
+        return array[k-1];
     }
     public static void main(String[] args)
     {
-        int arr[] = {3,5,4,2,9};
+        int array[] = {3,5,4,2,9};
         int k = 4;
-        System.out.println(kthSmallestNum(arr,k));
+        System.out.println(kthSmallestNum(array,k));
     }
 }
