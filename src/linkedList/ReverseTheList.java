c62fe166d0652da650b08fb041db77b8571a9744
Aastha Kumari <aasthak108@gmail.com>
2022-10-28 23:49:53 +0530

    update

---
+++ src/linkedList/ReverseTheList.java
@@ -0,0 +1,40 @@
+package linkedList;
+public class ReverseTheList {
+    static Node head;
+    public static Node reverseTheList(Node head)
+    {
+        Node prev = null;
+        Node nextNode = null;
+        Node curr = head;
+        while(curr!=null)
+        {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        head = prev;
+        return head;
+    }
+    static void printList(Node head)
+    {
+        Node curr = head;
+        while(curr != null)
+        {
+            System.out.println(" " + curr.data);
+            curr = curr.next;
+        }
+    }
+    public static void main(String[] args)
+    {
+        ReverseTheList list = new ReverseTheList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.head.next.next.next = new Node(4);
+        list.reverseTheList(head);
+        head = list.reverseTheList(head);
+        list.printList(head);
+    }
+
+}
