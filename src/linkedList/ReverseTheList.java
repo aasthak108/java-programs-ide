fe402bda8cfa38cfcc56394e10c33754253cc174
Aastha Kumari <aasthak108@gmail.com>
2022-11-05 22:38:10 +0530

    update

--- src/linkedList/ReverseTheList.java
+++ src/linkedList/ReverseTheList.java
@@ -29,7 +29,7 @@ public class ReverseTheList {
     {
         ReverseTheList list = new ReverseTheList();
         list.head = new Node(1);
-        list.head.next = new Node(2);
+        list.head.next = new Node(5);
         list.head.next.next = new Node(3);
         list.head.next.next.next = new Node(4);
         list.head=list.reverseTheList(list.head);
