2a888fd318a63a5281cce13ba0a867488347e3f3
aastha <aasthak108@gmail.com>
2023-02-12 10:42:07 +0530

    update

---
+++ src/BST/FindMinElement.java
@@ -0,0 +1,30 @@
+package BST;
+
+public class FindMinElement {
+    public static int findMinElement(Node node)
+    {
+        Node curr=node;
+        if(curr == null)
+        {
+            return 0;
+        }
+        if(curr.left==null)
+        {
+            return curr.data;
+        }
+        while(curr.left!= null)
+        {
+            curr=curr.left;
+        }
+        return curr.data;
+    }
+    public static void main(String[] args)
+    {
+        Node root = new Node(25);
+        Insert add = new Insert();
+        add.insert(root,26);
+        add.insert(root,27);
+        System.out.println(findMinElement(root));
+    }
+
+}
--- src/BST/SearchInBst.java
+++ src/BST/SearchInBst.java
@@ -9,17 +9,16 @@ class Node {
         right = null;
     }
 }
-public class SearchInBst {
-    static void insert(Node node,int data)
-    {
-        if(data<node.data) {
+ class Insert {
+    static void insert(Node node, int data) {
+        if (data < node.data) {
             if (node.left != null) {
                 insert(node.left, data);
             } else {
                 node.left = new Node(data);
             }
         }
-        if(data>node.data) {
+        if (data > node.data) {
             if (node.right != null) {
                 insert(node.right, data);
             } else {
@@ -27,33 +26,34 @@ public class SearchInBst {
             }
         }
     }
-    static int searchInBst(Node node,int x)
-    {
-        if(node==null)
-        {
+}
+
+    public class SearchInBst {
+        static int searchInBst(Node node, int x) {
+            if (node == null) {
+                return -1;
+            }
+            if (x == node.data) {
+                return 1;
+            }
+            if (x > node.data) {
+                return searchInBst(node.right, x);
+            }
+            if (x < node.data) {
+                return searchInBst(node.left, x);
+            }
             return -1;
         }
-        if(x==node.data)
-        {
-           return 1;
+
+        public static void main(String[] args) {
+            Node root = new Node(24);
+            Insert add = new Insert();
+            add.insert(root, 12);
+            add.insert(root, 45);
+            add.insert(root, 34);
+            add.insert(root, 1);
+            System.out.println(searchInBst(root, 1));
         }
-        if(x>node.data)
-        {
-            return searchInBst(node.right,x);
-        }
-        if(x<node.data){
-            return searchInBst(node.left, x);
-        }
-        return -1;
-    }
-    public static void main(String[] args)
-    {
-        Node root = new Node(24);
-        insert(root,12);
-        insert(root,45);
-        insert(root,34);
-        insert(root,1);
-        System.out.println(searchInBst(root,1));
+
     }
 
-}
