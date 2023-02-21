14725439579d3843d944d2abecf099899d169d40
aastha <aasthak108@gmail.com>
2023-02-22 00:47:39 +0530

    update

---
+++ src/graph/AdjMatrix.java
@@ -0,0 +1,36 @@
+package graph;
+
+public class AdjMatrix {
+    int vertex;
+    int mat[][];
+    AdjMatrix(int v)
+    {
+        vertex = v;
+        mat= new int[vertex][vertex];
+    }
+    public void addEdge(int source,int destination)
+    {
+        mat[source][destination]=1;
+        mat[destination][source]=1;
+    }
+    public void printGraph()
+    {
+        for(int i =0;i<vertex;i++)
+        {
+            for(int j=0;j<vertex;j++)
+            {
+                System.out.print(" " + mat[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args)
+    {
+        AdjMatrix adj = new AdjMatrix(5);
+        adj.addEdge(0,1);
+        adj.addEdge(1,2);
+        adj.addEdge(2,3);
+        adj.addEdge(3,4);
+        adj.printGraph();
+    }
+}
