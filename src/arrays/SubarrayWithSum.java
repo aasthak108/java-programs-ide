4fd6009f0a89d1736ac5720d3b32040d2a718cca
aastha <aasthak108@gmail.com>
2023-03-15 11:39:54 +0530

    update

--- src/arrays/SubarrayWithSum.java
+++ src/arrays/SubarrayWithSum.java
@@ -1,34 +1,38 @@
 package arrays;
 
 import java.util.ArrayList;
-//returning(output) the range of SubArray;
-public class SubarrayWithSum {
-    public static void subarrayWithSum(int arr[],int sum)
-    {
-        ArrayList<Integer> list = new ArrayList<>();
-        int l=0;
-        int r=0;
-        int s =0;
-        int [] arr1 = new int[arr.length];
-        while(l<=r && s<sum) {
-            s = s + arr[r];
-            r++;
-        }
-           while(l<=r && s>sum)
-            {
-                s = s-arr[l];
-                l++;
+    //returning(output) the range of SubArray;
+    public class SubarrayWithSum {
+        public static void subarrayWithSum(int arr[],int sum)
+        {
+            ArrayList<Integer> list = new ArrayList<>();
+            int l=0;
+            int r=0;
+            int s =0;
+            for(int i=0;i<arr.length;i++) {
+                s = s + arr[i];
+                r = i;
+                while (s > sum) {
+                    s = s - arr[l];
+                    l++;
+                }
+                if (s == sum) {
+                   break;
+                }
             }
-            if(s==sum)
+            if(s!=sum)
             {
+                list.add(-1);
+            }
+            else {
                 list.add(l);
                 list.add(r);
             }
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i) + " ");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i) + " ");
+            }
         }
-    }
-    public static void main(String[] args)
+        public static void main(String[] args)
     {
         int arr[] = {1,4,20,3,10,5};
         int sum =33;
