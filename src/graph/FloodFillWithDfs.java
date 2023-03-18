1e7b56a824f2a0822be6a1ef64e39cf0da841f11
aastha <aasthak108@gmail.com>
2023-03-18 19:28:12 +0530

    update

--- src/graph/FloodFillWithDfs.java
+++ src/graph/FloodFillWithDfs.java
@@ -7,27 +7,27 @@ public class FloodFillWithDfs {
                 if( newMatrix[sr][sc]!=oldColor)
                     return;
                 newMatrix[sr][sc]=newColor;
-            dfs(newMatrix, sr - 1, sc,3,2); // up
-            dfs(newMatrix, sr + 1, sc,3,2); // down
-            dfs(newMatrix, sr, sc - 1,3,2); // left
-            dfs(newMatrix, sr, sc + 1,3,2); // right
+            dfs(newMatrix, sr - 1, sc,2,1); // up
+            dfs(newMatrix, sr + 1, sc,2,1); // down
+            dfs(newMatrix, sr, sc - 1,2,1); // left
+            dfs(newMatrix, sr, sc + 1,2,1); // right
         }
         public int[][] floodFill(int[][] newMatrix,int sr,int sc,int newColor) {
                 int[][] ans = newMatrix;
                int x = ans.length;
                int y = ans[0].length;
-               int oldColor = 2;
-               newColor = 3;
+               int oldColor = 1;
+               newColor = 2;
                oldColor = newMatrix[sr][sc];
-            dfs(ans,1,1,3,2);
+            dfs(ans,1,1,2,1);
                return ans;
 
             }
             public static void main(String[] args) {
-                int[][]  newMatrix = {{1, 0, 0}, {1, 2, 2}, {2, 2, 0}};
+                int[][]  newMatrix = {{1,1,1}, {1, 1, 0}, {1, 0, 1}};
                 //boolean[][] visited = new boolean[newMatrix.length][newMatrix[0].length];
                 FloodFillWithDfs obj = new FloodFillWithDfs();
-                int [][]answer = obj.floodFill(newMatrix,1,1,3);
+                int [][]answer = obj.floodFill(newMatrix,1,1,2);
                 for (int i = 0; i < answer.length; i++) {
                     for (int j = 0; j < answer[0].length; j++) {
                         System.out.print(" " +answer[i][j]);
