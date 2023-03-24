289203ac0e74da6d2be7fe17bf23feb187a6d428
aastha <aasthak108@gmail.com>
2023-03-24 22:59:09 +0530

    update

--- src/graph/NumberOfIslands.java
+++ src/graph/NumberOfIslands.java
@@ -33,11 +33,7 @@ public class NumberOfIslands {
 
     public static void main(String[] args)
     {
-        int matrix[][] =  {
-                {1, 1, 0, 0, 0},
-                {1, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 1}};
+        int matrix[][] =   {{1,0,1},{0,1,0},{1,1,1}};
         System.out.println(numberOfIslands(matrix,0,0));
     }
 }
