94f751a9125f460ca2c86ea93c20bb3740bc6a39
aastha <aasthak108@gmail.com>
2023-03-27 23:14:12 +0530

    update

---
+++ src/graph/NumberOfProvince.java
@@ -0,0 +1,36 @@
+package graph;
+
+public class NumberOfProvince {
+    public static int numberOfProvince(int [][]matrix, boolean visited[])
+    {
+        int count=0;
+        for(int i =0;i<matrix.length;i++)
+        {
+            if(!visited[i])
+            {
+                count++;
+                dfs(matrix,visited,i);
+            }
+        }
+        return count;
+
+    }
+    public static void dfs(int matrix[][],boolean[] visited, int vertex)
+    {
+        visited[vertex]= true;
+        for(int i =0;i<matrix[vertex].length;i++)
+        {
+            if(matrix[vertex][i]==1 && !visited[i]) {
+                dfs(matrix, visited, i);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[][] matrix = {{1,0,1},{0,1,0},{1,1,1}};
+        boolean visited[] = new boolean[matrix.length];
+        System.out.println(numberOfProvince(matrix,visited));
+    }
+
+}
