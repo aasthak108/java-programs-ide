5f4f3b441c6bd31e6dbba80b16c6baac540042ab
aastha <aasthak108@gmail.com>
2023-06-13 23:49:14 +0530

    update

--- src/arrays/TotalFood.java
+++ src/arrays/TotalFood.java
@@ -1,21 +1,24 @@
 package arrays;
 
 public class TotalFood {
-    public static int food(int arr[],int r,int unit) {
+    public static String food(int n ){
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
-            if (sum >= r * unit) {
-                return i + 1;
+            for(int i=1;i<n;i++)
+            {
+                if(n%i==0)
+                {
+                    sum = sum+i;
+                }
+                if(sum==n)
+                {
+                    return "YES";
+                }
             }
+            return "NO";
         }
-        return -1;
-    }
     public static void main(String[] args)
     {
-        int[] arr= {2 ,1, 3, 5, 7, 4, 1, 2};
-        int r = 7;
-        int unit =2;
-        System.out.println(food(arr,r,unit));
+        int n =28;
+        System.out.println(food(n));
     }
 }
