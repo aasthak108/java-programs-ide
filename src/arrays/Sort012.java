7bbb5291957a9790c6e6f2bcf4f9b5be63354491
aastha <aasthak108@gmail.com>
2023-06-14 11:15:15 +0530

    update

--- src/arrays/Sort012.java
+++ src/arrays/Sort012.java
@@ -1,27 +1,39 @@
 package arrays;
 
 public class Sort012 {
-    public static void  sort012(int arr[])
-    {
-        int temp;
-        for(int i=0;i<arr.length-1;i++)
-        {
-            if(arr[i]>arr[i+1])
-            {
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+    public static int[]  sort012(int arr[]) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                mid++;
+                low++;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else if (arr[mid] == 2) {
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+                high--;
+
             }
         }
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.print(" " +arr[i]);
-
-        }
+        return arr;
     }
     public static void main(String[] args)
     {
         int arr[] ={1,0,2,1,2,1};
-        sort012(arr);
+        int[] array = new Sort012().sort012(arr);
+        for(int i =0;i<array.length;i++)
+        {
+            System.out.print(" " + array[i]);
+        }
+        System.out.print("");
     }
 }
