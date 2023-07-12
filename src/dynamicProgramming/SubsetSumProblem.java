ac5d7a6af94d732c9268ebd15c367cc2941b00ee
aastha <aasthak108@gmail.com>
2023-07-13 00:44:07 +0530

    update

---
+++ src/dynamicProgramming/SubsetSumProblem.java
@@ -0,0 +1,32 @@
+package dynamicProgramming;
+
+public class SubsetSumProblem {
+
+    static boolean isSubset(int sum, int n, int arr[]) {
+    boolean t[][] = new boolean[sum+1][n+1];
+    for(int i=1;i<sum+1;i++){
+        t[i][0]=false;
+    }
+    for(int j=0;j<n+1;j++){
+        t[0][j]=true;
+    }
+    for(int i=1;i<sum+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(arr[j-1]<=i){
+                t[i][j]= t[i-arr[j-1]][j-1] || t[i][j-1];
+            }
+            else{
+                t[i][j]=t[i][j-1];
+            }
+        }
+    }
+    return t[sum][n];
+    }
+    public static void main(String[] args){
+        int arr[]= {3, 34, 4, 12, 5, 2};
+        int sum =3;
+        int n = arr.length;
+        System.out.println(""+ isSubset(sum,n,arr));
+
+    }
+}
