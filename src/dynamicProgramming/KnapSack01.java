ff7bd14d26078a479bbf43356183572523c481d0
aastha <aasthak108@gmail.com>
2024-03-04 20:19:16 +0530

    update

--- src/dynamicProgramming/KnapSack01.java
+++ src/dynamicProgramming/KnapSack01.java
@@ -2,47 +2,39 @@ package dynamicProgramming;
 
 import java.util.Arrays;
 
-public class KnapSack01 {
-    static int knapSack01(int W, int[] wt, int[] val, int n) {
+class KnapSakc01{
+
+    static int knapSack01(int wt[],int val[],int W, int n) {
         int dp[][] = new int[n + 1][W + 1];
         for (int i = 0; i < n + 1; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return utilityKnapSack01(W, wt, val, n, dp);
+        return utilityKS(dp,wt,val,W,n);
     }
+        static int utilityKS(int[][] dp,int wt[],int val[],int W, int n){
 
-    static int max(int a, int b) {
-        if (a > b) {
-            return a;
-        } else {
-            return b;
-        }
-    }
-
-    static int utilityKnapSack01(int W, int wt[], int val[], int n, int dp[][]) {
-        if (n == 0 || W == 0) {
+        if(n==0||W==0)
+        {
             return 0;
         }
-        if (dp[n][W] != -1) {
+        if(dp[n][W]!=-1){
             return dp[n][W];
         }
-        if (wt[n - 1] <= W) {
-            return dp[n][W] = max(val[n - 1] + utilityKnapSack01(W - wt[n - 1], wt, val, n - 1, dp),
-                    utilityKnapSack01(W, wt, val, n - 1, dp));
-        } else if (wt[n - 1] > W) {
-            return dp[n][W] = utilityKnapSack01(W, wt, val, n - 1, dp);
+        else if(wt[n-1]<=W){
+            return dp[n][W] = Math.max(val[n-1] + utilityKS(dp,wt,val,W-wt[n-1],n-1), utilityKS
+                    (dp,wt,val,W,n-1));
+        }
+        else if(wt[n-1]>W){
+            return dp[n][W] = utilityKS(dp,wt,val,W,n);
         }
         return 0;
-    }
-
-
+        }
     public static void main(String[] args){
         int wt[] = {10,20,30};
         int val[] = { 60, 100, 120 };
         int W=50;
         int n = wt.length;
-        System.out.println(knapSack01(W,wt,val,n));
-   }
-
+        System.out.println(knapSack01(wt,val,W,n));
+    }
 
 }
