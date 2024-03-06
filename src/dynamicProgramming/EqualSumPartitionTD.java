1dadf1269ded7205b2b8626d34052aaf88ea30a3
aastha <aasthak108@gmail.com>
2024-03-06 10:41:38 +0530

    update

--- src/dynamicProgramming/EqualSumPartitionTD.java
+++ src/dynamicProgramming/EqualSumPartitionTD.java
@@ -13,38 +13,32 @@ public class EqualSumPartitionTD {
     }
 
     static boolean check(int n, int sum, int[] arr) {
-        boolean t[][] = new boolean[n + 1][sum + 1];
-        boolean res = false;
-        for (int i = 1; i < n + 1; i++){
-            t[i][0] = false;
-    }
-        for(
-    int j = 0;
-    j<sum+1;j++)
-
-    {
-        t[0][j] = true;
-    }
-        for(int j = 1;j<n+1;j++)
-
-    {
-        for (int i = 1; i < sum + 1; i++) {
-            if (arr[j - 1] <= i) {
-                t[j][i] = t[j - 1][i - arr[j - 1]] || t[j - 1][i];
-            } else {
-                t[j][i] = t[j - 1][i];
-            }
+        boolean dp[][] = new boolean[n+1][sum+1];
+        for(int i =0;i<n+1;i++){
+            dp[i][0] = true;
         }
-    }
-        return t[n][sum];
+        for(int j=1;j<sum+1;j++){
+            dp[0][j] = false;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+
+        }
+        return dp[n][sum];
 }
 
 
     public static void main(String[] args){
-        int arr[] = {1,5,5,11};
+        int arr[] = {1,5,5,11,1,3};
         int n =arr.length;
         System.out.println(""+ canPartition(n,arr));
     }
 
 }
-
