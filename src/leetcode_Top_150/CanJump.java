358421d5c4bcd92cb0615bab59aaab3ca3dde10f
aastha <aasthak108@gmail.com>
2024-06-14 11:33:46 +0530

    update

--- src/leetcode_Top_150/CanJump.java
+++ src/leetcode_Top_150/CanJump.java
@@ -3,21 +3,22 @@ package leetcode_Top_150;
 public class CanJump {
     public static boolean canJump(int[] nums) {
         int count = 0;
-        int j = 0;
-        for (int i = 0; i < nums.length-1; i++) {
 
-            i = j;
-            count = nums[i];
-            j = j + count;
+        for (int i = 0; i < nums.length; i++) {
+
+            if(count<i){
+                return false;
+            }
+            count = Math.max(nums[i] + i, count);
 
         }
-        if (j+1 == nums.length || j+1 >= nums.length) {
+        if (count >= nums.length) {
             return true;
         }
-        return false;
+        return true;
     }
     public static void main(String args[]){
-        int nums [] = {4,2,1,0,4,9};
+        int nums [] = {1,0,1,0};
         System.out.println("" + canJump(nums));
     }
 }
