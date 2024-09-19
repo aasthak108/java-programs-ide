5c77299a9a70c84379118eab54d6a8192461b499
aastha <aasthak108@gmail.com>
2024-09-19 22:12:27 +0530

    updated the code

--- src/arrays/LeadersInArray.java
+++ src/arrays/LeadersInArray.java
@@ -24,7 +24,7 @@ public class LeadersInArray
     }
     public static void main(String args[])
     {
-        int arr[] = {16,17,4,3,5,2};
+        int arr[] = {16,17,4,3,5,1};
         System.out.println(""+findLeaders(arr));
     }
 }
