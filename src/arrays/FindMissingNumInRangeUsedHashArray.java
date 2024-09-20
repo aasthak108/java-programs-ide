8dab5f5fb183670d4a4922f544bd8aa516d38952
aastha <aasthak108@gmail.com>
2024-09-20 16:56:19 +0530

    Find Missing number in givev range in array [ 0 to n] used hash array

---
+++ src/arrays/FindMissingNumInRangeUsedHashArray.java
@@ -0,0 +1,27 @@
+package arrays;
+//used hash array to count the frequencies
+public class FindMissingNumInRangeUsedHashArray
+{
+    public static int findMissingNum(int arr[])
+    {
+        int n = arr.length;
+        int hash [] = new int[n+1];
+        for(int i =0;i<n-1;i++)
+        {
+            hash[arr[i]]++;
+        }
+        for(int i =1;i<=n;i++)
+        {
+            if(hash[i]==0)
+            {
+                 return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        int arr[] = {0,1,3};
+        System.out.println(""+ findMissingNum(arr));
+    }
+}
