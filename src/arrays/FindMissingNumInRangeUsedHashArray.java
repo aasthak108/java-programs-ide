a610424aa1b18edea63b8571839f3b17e72ac6e6
aastha <aasthak108@gmail.com>
2024-09-20 16:58:32 +0530

    Find missing Number in a given array from [0 to n], used hash array to count the frequency of all array elements

--- src/arrays/FindMissingNumInRangeUsedHashArray.java
+++ src/arrays/FindMissingNumInRangeUsedHashArray.java
@@ -21,7 +21,7 @@ public class FindMissingNumInRangeUsedHashArray
     }
     public static void main(String args[])
     {
-        int arr[] = {0,1,3};
+        int arr[] = {0,1,3,4};
         System.out.println(""+ findMissingNum(arr));
     }
 }
