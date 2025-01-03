ffdf35438cad6e170266f3e1b926ed90dfe565b7
aastha <aasthak108@gmail.com>
2025-01-03 23:42:14 +0530

    coin change problem solved using recursion

---
+++ src/dynamicProgramming/CoinChangeRec.java
@@ -0,0 +1,34 @@
+package dynamicProgramming;
+
+public class CoinChangeRec
+{
+    public static int count(int coins[], int sum)
+    {
+        return helper(coins, sum, coins.length);
+    }
+
+    public static int helper(int[] coins, int sum, int n)
+    {
+        if (sum == 0)
+        {
+            return 1;
+        }
+        if (sum < 0 || n == 0)
+        {
+            return 0;
+        }
+
+        // Recursive cases
+        int takeCoins = helper(coins, sum - coins[n - 1], n);
+        int notTakeCoins = helper(coins, sum, n - 1);
+
+        return takeCoins + notTakeCoins;
+    }
+
+    public static void main(String[] args)
+    {
+        int coins[] = {1, 2, 3};
+        int sum = 4;
+        System.out.println(count(coins, sum)); // Output: 4
+    }
+}
