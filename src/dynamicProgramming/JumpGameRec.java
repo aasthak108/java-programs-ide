7d1f99c4f3100e3415160cfb5b5f41252b82f01c
aastha <aasthak108@gmail.com>
2025-01-23 23:15:55 +0530

    optimized

--- src/dynamicProgramming/JumpGameRec.java
+++ src/dynamicProgramming/JumpGameRec.java
@@ -1,22 +1,28 @@
 package dynamicProgramming;
 
 public class JumpGameRec {
-    public static boolean canReach(int[] arr, int curr) {
-        if (curr >= arr.length - 1) {
+    public static boolean canReach(int[] arr, int curr)
+    {
+        if (curr >= arr.length - 1)
+        {
             return true;
         }
-        if (arr[curr] == 0) {
+        if (arr[curr] == 0)
+        {
             return false;
         }
-        for (int j = 1; j <= arr[curr]; j++) {
-            if (canReach(arr, curr + j)) {
+        for (int j = 1; j <= arr[curr]; j++)
+        {
+            if (canReach(arr, curr + j))
+            {
                 return true;
             }
         }
         return false;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int arr[] = {2,2,1,1,1};
         System.out.println("" + canReach(arr, 0));
     }
